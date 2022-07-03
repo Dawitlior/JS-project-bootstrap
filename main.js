be52df5a8fc9ad26cb991ce53415643c570aa829
Dawitlior <dawitlior777@gmail.com>
2022-07-03 09:45:11 +0300

    add

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 const BASIC_API ="https://my-json-server.typicode.com/Jeck99/fake-server/devices";
 
-async function someGetFunction(){
-  try {
-    return await fetch("https://my-json-server.typicode.com/Jeck99/fake-server/devices")
-    .then((response) => response.json());
-  } catch (error1) {
-    alert(error1);
-  } finally {
-  }
 
+
+function cardTemplate(devices){
+  return `<div class="dawitCard">
+  <div>brand : ${devices.brand}</div>
+  <div>color : ${devices.color}</div>
+  <div>price : ${devices.price}</div>
+  </div>`
+}
+
+async function someGetFunction(htmlFromTemplate = ""){
+let devices = await fetch(BASIC_API).then(res=>res.json())
+devices.forEach(devices =>{
+  htmlFromTemplate+=cardTemplate(devices)
+});
+document.getElementById("myDivTabPhone").innerHTML = htmlFromTemplate;
 }
 someGetFunction()
 
@@ -16,42 +23,40 @@ someGetFunction()
 
 
 
-
-
-async function postTheMobileFunction() {
-  try {
-    // const data = {
-      // phone: {
-      // brand: 
-      // color: 
-      // createdAt: 
-      // id: 
-      // isAvailable: 
-      // picture: 
-      // price: 
-      // ram:
-    // },
-  // };
-  return await fetch(BASIC_API, {
-    method: "POST",
-    body: JSON.stringify(data),
-    headers: { "content-type": "application/json" },
-  });
-} 
-catch (error) {
-  alert("sorry about the error");} 
-  finally {}
-}
+// async function postTheMobileFunction() {
+//   try {
+//     // const data = {
+//       // phone: {
+//       // brand: 
+//       // color: 
+//       // createdAt: 
+//       // id: 
+//       // isAvailable: 
+//       // picture: 
+//       // price: 
+//       // ram:
+//     // },
+//   // };
+//   return await fetch(BASIC_API, {
+//     method: "POST",
+//     body: JSON.stringify(data),
+//     headers: { "content-type": "application/json" },
+//   });
+// } 
+// catch (error) {
+//   alert("sorry about the error");} 
+//   finally {}
+// }
 
 
 
-let myDiv = document.getElementById("theMainDiv");
-let counter = 0;
-function printTheItemsOnScreen(){
-someGetFunction().then((result)=>{
-  result.forEach(item => {
-    myDiv.innerHTML += 
-  });
-})
-}
-printTheItemsOnScreen();
+// let myDiv = document.getElementById("theMainDiv");
+// let counter = 0;
+// function printTheItemsOnScreen(){
+// someGetFunction().then((result)=>{
+//   result.forEach(item => {
+//     myDiv.innerHTML += 
+//   });
+// })
+// }
+// printTheItemsOnScreen();
