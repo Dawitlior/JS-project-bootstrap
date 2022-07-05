872c54395a2b4531f24d81f103ef683fc3b3d89a
Dawitlior <dawitlior777@gmail.com>
2022-07-05 14:44:33 +0300

    add

---
+++ javascript/Phone-Tab.js
@@ -0,0 +1,43 @@
+const BASIC_API =
+  "https://my-json-server.typicode.com/Jeck99/fake-server/devices";
+const SOME_IMG =
+  "https://www.digitaltrends.com/wp-content/uploads/2021/09/iphone-13-pro-review-dan-baker-35.jpg?p=1";
+
+async function getCardsAndPrint() {
+  let myMainPhoneTabDiv = document.getElementById("phoneTabId");
+  const MY_LOADING_GIPY = document.getElementById("loading");
+  try {
+    MY_LOADING_GIPY.style.display = true;
+    return await fetch(BASIC_API)
+      .then((res) => res.json())
+      .then((res) => {
+        res.forEach((element) => {
+          myMainPhoneTabDiv.innerHTML += `<div class="card col-md-4 col-sm-12 ">
+   <div>
+   <img class="card-img col-md-4" src="${SOME_IMG}">
+  </div>
+   <div>brand : ${element.brand}</div>
+    <div>createdAt : ${element.createdAt}</div>
+   <div>color : ${element.color}</div>
+   <div>ram : ${element.ram}</div>
+   <div>isAvailable : ${element.isAvailable}</div>
+   <div>price : ${element.price}</div>
+   <div><button type="submit" class="btn btn-warning"><strong>I'm intrusting </strong></button></div>
+   <br><br><br>
+   </div>
+   `;
+        });
+      });
+  } catch (error) {
+    alert(error);
+  } finally {
+    MY_LOADING_GIPY.style.display = false;
+  }
+}
+ getCardsAndPrint();
+
+ async function getTheUsersFunction(){
+  return await fetch(BASIC_API)
+  .then(res=>console.log(res.json()))
+}
+getTheUsersFunction();
--- main.js
+++ main.js
@@ -1,32 +1,56 @@
-const BASIC_API = "https://my-json-server.typicode.com/Jeck99/fake-server/devices";
-const SOME_IMG = "https://www.digitaltrends.com/wp-content/uploads/2021/09/iphone-13-pro-review-dan-baker-35.jpg?p=1";
-
-async function someGetFunction(htmlFromTemplate = "") {
-  let devices = await fetch(BASIC_API).then((res) => res.json());
-  devices.forEach((devices) => {
-    htmlFromTemplate += cardTemplate(devices);
-  });
-  document.getElementById("myDivTabPhone").innerHTML = htmlFromTemplate;
-}
-someGetFunction();
 
 
-function cardTemplate(devices) {
-  return (phoneTabId.innerHTML += `<div class="card col-md-4 col-sm-12 ">
-  <div>
-  <img class="card-img col-md-4" src="${SOME_IMG}">
-  </div>
-  <div>brand : ${devices.brand}</div>
-  <div>brand : ${devices.createdAt}</div>
-  <div>color : ${devices.color}</div>
-  <div>brand : ${devices.ram}</div>
-  <div>brand : ${devices.isAvailable}</div>
-  <div>price : ${devices.price}</div>
-  <br><br><br>
-  </div>
-  `);
-}
-cardTemplate();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// async function someGetFunction(htmlFromTemplate = "") {
+//   let devices = await fetch(BASIC_API).then((res) => res.json());
+//   devices.forEach((devices) => {
+//     htmlFromTemplate += cardTemplate(devices);
+//   });
+//   document.getElementById("myDivTabPhone").innerHTML = htmlFromTemplate;
+// }
+// someGetFunction();
+
+
+// function cardTemplate(devices) {
+//   return (phoneTabId.innerHTML += `<div class="card col-md-4 col-sm-12 ">
+//   <div>
+//   <img class="card-img col-md-4" src="${SOME_IMG}">
+//   </div>
+//   <div>brand : ${devices.brand}</div>
+//   <div>brand : ${devices.createdAt}</div>
+//   <div>color : ${devices.color}</div>
+//   <div>brand : ${devices.ram}</div>
+//   <div>brand : ${devices.isAvailable}</div>
+//   <div>price : ${devices.price}</div>
+//   <br><br><br>
+//   </div>
+//   `);
+// }
+// cardTemplate();
 
 
 
