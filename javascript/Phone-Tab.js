7880da6457800dc644e27967670a7cfa0b46f02c
Dawitlior <dawitlior777@gmail.com>
2022-07-06 18:40:56 +0300

    add

--- javascript/Phone-Tab.js
+++ javascript/Phone-Tab.js
@@ -1,25 +1,35 @@
 const BASIC_API = "https://my-json-server.typicode.com/Jeck99/fake-server/devices";
 const SOME_IMG = "https://www.digitaltrends.com/wp-content/uploads/2021/09/iphone-13-pro-review-dan-baker-35.jpg?p=1";
 let arrayImg = [
-  "../images/array-images-tabs/",
-  "../images/array-images-tabs/",
-  "../images/array-images-tabs/",
+  "../images/array-images-tabs/sumsong2.webp",
+  "../images/array-images-tabs/sumsong4.webp",
+  "../images/array-images-tabs/apple1.webp",
+  "../images/array-images-tabs/mi1.webp",
+  "../images/array-images-tabs/sony1.webp",
+  "../images/array-images-tabs/google.1.webp",
+  "../images/array-images-tabs/apple2.webp",
+  "../images/array-images-tabs/apple3.webp",
+  "../images/array-images-tabs/sony2.webp",
+  "../images/array-images-tabs/apple4.webp",
+  "../images/array-images-tabs/apple5.webp",
+  "../images/array-images-tabs/google.2.webp",
+  "../images/array-images-tabs/sony3.webp",
+  "../images/array-images-tabs/mi2.webp",
+  "../images/array-images-tabs/apple6.webp"
 ];
 let counter = 0;
 async function getCardsAndPrint() {
   let myMainPhoneTabDiv = document.getElementById("phoneTabId");
   const MY_DIV = document.getElementById("loading");
   try {
-    MY_DIV.innerHTML =
-      "<img class='rounded mx-auto d-block' style='width:10vw' src='../images/waitingGif/loading2.gif' />"; 
+    MY_DIV.innerHTML = "<img src='../images/waitingGif/loading2.gif' class='rounded mx-auto d-block' style='width:10vw' />"; 
     return await fetch(BASIC_API)
-      .then((res) => res.json())
-      .then((res) => {
-        res.forEach((element) => {
-          myMainPhoneTabDiv.innerHTML += `<div class="card col-md-4 col-sm-12  bg-black text-white text-start  ">
-
+      .then(res => res.json())
+      .then((response) => {
+        response.forEach((element) => {
+          myMainPhoneTabDiv.innerHTML += `<div class="card col-md-4 col-sm-12 bg-black text-white text-start ">
    <div>
-   <img class="card-img" src="${SOME_IMG}">
+   <img class="card-img" src="${arrayImg[counter++]}">
   </div>
   <div> id : ${element.id} </div>
 
@@ -35,7 +45,7 @@ async function getCardsAndPrint() {
 
    <div>price : ${element.price}</div>
 
-   <div><button class="btn btn-warning" type="button"><strong>I'm intrusting </strong></button></div>
+   <div><button class="btn btn-danger" type="button"><strong>Delete</strong></button></div>
    
    </div>
    `;
--- javascript/all-information.js
+++ javascript/all-information.js
@@ -1,20 +1,25 @@
 const USERS_API = "https://my-json-server.typicode.com/Jeck99/fake-server/users";
 
 async function getTheUsersFunction() {
-  let myContainer = document.getElementById("myTableId");
+  let myContainer = document.getElementById("DivContainer");
   try {
     return await fetch(USERS_API)
       .then((res) => res.json())
       .then((res) => {
         res.forEach((res) => {
           myContainer.innerHTML += `
-          <tbody class=''>
-          <td>${res._id}</td>
-          <td>${res.age}</td>
-          <td>${res.name.last}</td>
-          <td>${res.name.first}</td>
-          <td>${res.email}</td>
-          <td>${res.phone}</td>
+          
+          <div class='card col-md-4 col-sm-12 bg-white bg-opacity-50 text-black text-center d-flex'>
+          <div>
+          <img class='card-img' src='../images/all-information-images/user-pic.1.png'
+          </div>
+          <div>Id :${res._id}</div>
+          <div>age : ${res.age} </div>
+          <div>last name:${res.name.last}</div>
+          <div>first name:${res.name.first}</div>
+          <div>Email:${res.email}</div>
+          <div>Phone:${res.phone}</div>   
+          
           `;
         });
       });
@@ -24,3 +29,35 @@ async function getTheUsersFunction() {
   }
 }
 getTheUsersFunction();
+
+
+
+// `<div class="card col-md-4 col-sm-12  bg-black text-white text-start ">
+//    <div>
+//    <img class="card-img" src="${SOME_IMG}">
+//   </div>
+//   <div> id : ${element.id} </div>
+
+//    <div>brand : ${element.brand}</div>
+
+//     <div>createdAt : ${element.createdAt}</div>
+
+//    <div>color : ${element.color}</div>
+
+//    <div>ram : ${element.ram}</div>
+
+//    <div>isAvailable : ${element.isAvailable}</div>
+
+//    <div>price : ${element.price}</div>
+
+//    <div><button class="btn btn-danger" type="button"><strong>Delete</strong></button></div>
+   
+//    </div>
+
+//  <td>${res._id}</td>
+//           <td>${res.age}</td>
+//           <td>${res.name.last}</td>
+//           <td>${res.name.first}</td>
+//           <td>${res.email}</td>
+//           <td>${res.phone}</td>
+//           </tbody>
